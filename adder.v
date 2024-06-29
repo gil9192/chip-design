f87c440ad5e3f1c012c741b2a1efddf48d9ad7e2
gil9192 <gil9192@gmail.com>
2024-06-29 15:18:56 +0300

    Adder Lab  automatic test

--- adder.v
+++ adder.v
@@ -1,17 +1,27 @@
-module adder (
-    output reg [4:0] Sum,
-    input [3:0] A,
-    input [3:0] B,
+module adder #(
+    parameter INPUT_WIDTH = 4
+)
+(
+    output [INPUT_WIDTH:0] Sum,
+    input [INPUT_WIDTH-1:0] A,
+    input [INPUT_WIDTH-1:0] B,
     input enable,
     input clk,
-    input rst);
+    input rst
+);
+    reg [INPUT_WIDTH:0] Temp;
+    reg [INPUT_WIDTH:0] Out;
+
+    assign Sum = Temp;
 
     always @(posedge clk, posedge rst) begin
         if (rst) begin
-            Sum <= 0;
-        end 
-        else if(enable) begin
-            Sum <= A + B;
+            Temp <= 0;
+        end
+        else begin
+            if (enable) begin
+                Temp <= A + B;
+            end
         end
     end
 endmodule
--- adder_tb.v
+++ adder_tb.v
@@ -1,44 +1,103 @@
 `timescale 1ps/1ps
 
 module adder_tb;
-    wire [4:0] t_Sum;
-    reg [3:0] t_A;
-    reg [3:0] t_B;
-    reg t_enable;
-    reg t_clk;
-    reg t_rst;
+    reg tb_clk;
+    wire [4:0] tb_sum;
+    wire [4:0] tb_ref_sum;
+    reg [3:0] tb_A;
+    reg [3:0] tb_B;
+    reg tb_enable;
+    reg tb_rst;
+    reg tb_mismatches;
+    reg [4:0] tb_tmp;
+    
+    event rst_start;
+    event rst_end;
+    event end_sim;
+    event terminate_sim;
 
-    adder dut1 (.Sum(t_Sum), .A(t_A), .B(t_B), .enable(t_enable), .clk(t_clk), .rst(t_rst));
 
-    initial begin
-        t_A = 0;
-        t_B = 0;
-        t_clk = 0;
-        t_rst = 0;
-        t_enable = 0;
-        #10 forever #10 t_clk = !t_clk;
+    adder dut (.Sum(tb_sum), .A(tb_A), .B(tb_B), .enable(tb_enable), .clk(tb_clk), .rst(tb_rst));
+
+
+
+    initial begin : INIT
+        tb_A = 0;
+        tb_B = 0;
+        tb_rst = 0;
+        tb_enable = 0;
+        tb_mismatches = 0;
     end
 
-    initial	// Test stimulus
-    begin
-        #0
-        #20 t_rst = 1;
-        #20 t_rst = 0;
-        #20 t_A = 1;
-        #20 t_B = 2;
-        #20 t_enable = 1;
-        #20 t_enable = 0;
-        #20 t_A = 2;
-        #20 t_B = 2;
-        #20 t_enable = 1;
-        #20 t_enable = 0;
-        #20 t_rst = 1;
-        #20 t_rst = 0;
-        #20 t_A = 2;
-        #20 t_B = 1;
-        #20 t_enable = 1;
-        #20 t_rst = 1;
-        #20 t_rst = 0;
-        #100 $stop;
+    initial begin : CLOCK
+        tb_clk = 0;
+        forever begin
+            #10 tb_clk = !tb_clk;
+        end
+    end
+
+    initial begin : RESET
+        forever begin
+            @(rst_start);
+            $display("Reset triggered at: %d", $time);
+            @(negedge tb_clk);
+            tb_rst = 1;
+            @(negedge tb_clk);
+            tb_rst = 0;
+            -> rst_end;
+        end
+    end
+
+    initial begin : TEST_CASE
+        #10 -> rst_start;
+        @(rst_end);
+        #20;
+        fork
+            repeat (20) begin
+                @(posedge tb_clk)
+                tb_A = $random;
+                tb_B = $random;
+                $display("A: %d B: %d", tb_A, tb_B);
+            end
+            repeat (20) begin
+                @(posedge tb_clk)
+                tb_enable = $random;
+            end
+        join
+        -> terminate_sim;
+    end
+
+    initial @(terminate_sim) begin : TERMINATION
+        $display("############################");
+        if (tb_mismatches) begin
+            $display("Tesbench status: Failed");
+        end
+        else begin
+            $display("Tesbench status: Passed");
+        end
+        $display("Number of mismatches: %d", tb_mismatches);
+        $display("############################");
+        $finish;
+    end
+
+    assign tb_ref_sum = tb_tmp;
+
+    always @(posedge tb_clk, posedge tb_rst) begin
+        if (tb_rst) begin
+            tb_tmp <= 0;
+        end
+        else begin
+            if (tb_enable) begin
+                tb_tmp <= tb_A + tb_B;
+            end
+        end
+    end
+
+    initial begin : CHECKER
+        if (tb_ref_sum != tb_sum) begin
+            $display("Found mismatch at: %d", $time);
+            $display("Expecting: %d", tb_ref_sum, "Got", tb_sum);
+            tb_mismatches = tb_mismatches + 1;
+        end
     end
 endmodule
